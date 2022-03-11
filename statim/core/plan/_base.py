abcd9d2c1c818aa15536fe0c3ab03ca08ed9bf21
Tom Hunze <github@thunze.de>
2022-03-11 15:21:49 +0100

    Added field constraints and documentation
    - Also replaced Bitness with a bool field for Windows 10

--- statim/core/plan/_base.py
+++ statim/core/plan/_base.py
@@ -1,21 +1,25 @@
 """Base classes and related classes used across the OS-specific modules."""
 
 from abc import ABC, abstractmethod
+from collections import namedtuple
 from enum import Enum
 from pathlib import Path
 from typing import Any, Literal, Optional
 
 from pydantic import AnyHttpUrl
 from pydantic import BaseModel as PydanticBaseModel
-from pydantic import Extra
+from pydantic import Extra, Field
 
 __all__ = [
-    'BaseModel',
     'OS',
-    'Plan',
+    'BaseModel',
     'LocalSource',
     'RemoteSource',
+    'Plan',
     'PlanWithUnattendSupport',
+    'OS_FIELD_KWARGS',
+    'SOURCE_FIELD_KWARGS',
+    'UNATTEND_FIELD_KWARGS',
 ]
 
 
@@ -31,23 +35,44 @@ class BaseModel(PydanticBaseModel):
     """
 
     class Config:
+        """Pydantic model configuration."""
+
         allow_mutation = False
         schema_extra = {'additionalProperties': False}
-        extra = Extra.forbid  # insert reason here
+        extra = Extra.forbid
 
 
 class LocalSource(BaseModel):
     """Source of an OS installation image accessible via a local path."""
 
     type: Literal['local']
-    path: Path
+
+    path: Path = Field(
+        ...,
+        title='Path',
+        description=(
+            'Local path to an installation image for the desired operating system. '
+            'The referenced image should correspond to additional settings eventually '
+            'set in this configuration. For example: "/path/to/image.iso".'
+        ),
+    )
 
 
 class RemoteSource(BaseModel):
     """Source of an OS installation image accessible via HTTP."""
 
     type: Literal['remote']
-    url: AnyHttpUrl
+
+    url: AnyHttpUrl = Field(
+        ...,
+        title='URL',
+        description=(
+            'HTTP URL of an installation image for the desired operating system. Not '
+            'constrained to public TLDs or domain names in general. The referenced '
+            'image must correspond to additional settings eventually set in this '
+            'configuration.'
+        ),
+    )
 
 
 class Plan(BaseModel, ABC):
@@ -56,7 +81,14 @@ class Plan(BaseModel, ABC):
     # dummy field for linters, gets correctly overridden by every subclass
     os: OS
 
-    uefi: bool
+    uefi: bool = Field(
+        ...,
+        title='UEFI Support',
+        description=(
+            'If set to true, the resulting drive will be bootable from a UEFI BIOS '
+            'instead of a legacy BIOS.'
+        ),
+    )
 
     @abstractmethod
     def resolve_source(self) -> LocalSource | RemoteSource:
@@ -68,13 +100,14 @@ class Plan(BaseModel, ABC):
         """
 
     def pre_copy(self) -> None:
-        """Method invoked *before* copying the required OS installation files from
-        the image to the drive. Empty by default.
+        """Method invoked *before* writing the required OS installation files to the
+        drive. Empty by default.
         """
 
     def post_copy(self) -> None:
-        """Method invoked *after* copying the required OS installation files from the
-        image to the drive. Empty by default.
+        """Method invoked *after* writing the required OS installation files,
+        including possibly required files for an unattended installation, to the
+        drive. Empty by default.
         """
 
 
@@ -94,3 +127,24 @@ class PlanWithUnattendSupport(Plan):
         This method is invoked after copying the required OS installation files from
         the image to the drive but *before* running ``post_copy``.
         """
+
+
+_FieldMeta = namedtuple('_FieldMeta', ['title', 'description'])
+"""Keyword arguments to pass to pydantic's ``Field`` function when used for the
+definition of an overridden field of an OS-specific ``Plan``.
+"""
+
+OS_FIELD_KWARGS = _FieldMeta(
+    title='Operating System',
+    description='Operating system whose installation files to write to the drive.',
+)
+
+SOURCE_FIELD_KWARGS = _FieldMeta(
+    title='Source',
+    description='Source of the installation files for the desired operating system.',
+)
+
+UNATTEND_FIELD_KWARGS = _FieldMeta(
+    title='Unattended Installation',
+    description='Configuration of an unattended installation of the operating system.',
+)
