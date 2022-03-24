f4c212c32558246fdc8f7b430c08a6e90c699077
Tom Hunze <github@thunze.de>
2022-03-24 18:04:18 +0100

    First draft of the image module

--- statim/__main__.py
+++ statim/__main__.py
@@ -9,9 +9,11 @@ def main() -> None:
         level=logging.DEBUG,
         stream=sys.stdout,
         style='{',
-        format='{asctime:<19}  {levelname:<8}  {name:<25}  {message}',
+        format='{asctime:<19}  {threadName:<24}  {levelname:<8}  {name:<20}  {message}',
         datefmt='%Y-%m-%d %H:%M:%S',
     )
+    logging.getLogger('requests').setLevel(logging.INFO)
+    logging.getLogger('urllib3').setLevel(logging.INFO)
 
 
 if __name__ == '__main__':
